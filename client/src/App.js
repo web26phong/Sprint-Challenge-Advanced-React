f026181ca87431f67ada69bd30ac45e8ab26174c
Phong Yang <phongyang88@hotmail.com>
2020-01-17 10:53:24 -0600

    data retrieved and displayed

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import Axios from "axios";
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    players: []
+  }
+
+  componentDidMount(){
+    console.log("app mounted")
+    Axios
+    .get(`http://localhost:5000/api/players`)
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        players: res.data
+        
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
 
   render(){
+    console.log("app rendered")
     return (
     <div className="App">
-      
+      <h1>Women's World Cup Players Search Interest from Google Trends</h1>
+      <div className="allPlayersContainer">
+        {this.state.players.map(player => (
+          <div className="playerCard" key={player.id}>
+            <h2>{player.name}</h2>
+            <h3>Country: {player.country}</h3>
+            <h3>Searches: {player.searches}</h3>
+          </div>
+        ))}
+      </div>
     </div>
     );
   }
